f1db850bffb9bb2cc3ad4116445bb642061303d9
Amorn Apichattanakul <benamorn@gmail.com>
2022-11-22 18:47:22 +0700

    add code for sprite sheet

---
+++ lib/sprite_sheet.dart
@@ -0,0 +1,46 @@
+import 'package:flame/game.dart';
+import 'package:flame/input.dart';
+import 'package:flame/sprite.dart';
+import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
+import 'package:flutter/animation.dart';
+
+class SpriteSheetWidget extends FlameGame with TapDetector {
+  @override
+  void onTapDown(TapDownInfo info) {
+    print(info.eventPosition.game);
+  }
+
+  @override
+  Future<void> onLoad() async {
+    final spriteSheet = SpriteSheet(
+      image: await images.load('cat_sprite_long.png'),
+      srcSize: Vector2(50.0, 50.0),
+    );
+    final spriteSize = Vector2(50.0, 50.0);
+
+    final animation =
+        spriteSheet.createAnimation(row: 0, stepTime: 0.1, to: 60);
+    final component1 = SpriteAnimationComponent(
+      animation: animation,
+      scale: Vector2(0.4, 0.4),
+      position: Vector2(160, -5),
+      size: spriteSize,
+    );
+
+    add(
+      component1
+        ..add(
+          MoveEffect.to(
+            Vector2(250, -5),
+            EffectController(
+              duration: 10,
+              reverseDuration: 10,
+              infinite: true,
+              curve: Curves.linear,
+            ),
+          ),
+        ),
+    );
+  }
+}
